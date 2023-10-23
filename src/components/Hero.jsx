0b4bbe0cca2a530864b3f71ad271bddbd27cf8f5
code <spyderidle@gmail.com>
2023-10-23 16:32:22 +0530

    correcting github url

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="logo" className="w-28 object-contain" />
 
-        <button type="button" onClick={()=>window.open('https://github.com/spyderidle')} className="black_btn">Github</button>
+        <button type="button" onClick={()=>window.open('https://github.com/AnuragSingh014')} className="black_btn">Github</button>
       </nav>
       <h1 className="head_text"> 
         Summarize Articles with <br className="max-md:hidden "/>
